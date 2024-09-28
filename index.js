ced80f36d9b33679997869f541d905996aa28451
lorna gathumbi <nyawiralornah@gmail.com>
2024-09-28 11:10:32 +0300

    js looping

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    return names.map(names => `Thank you, ${names}, for the wonderful ${event} gift!`)
+}
+function countDown(num) {
+    while (num>=0) {
+        console.log(num);
+        num--;
+    }
+}
